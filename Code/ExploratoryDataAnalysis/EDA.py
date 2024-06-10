79d5f48ae8b92969d3814b35e81fe62c550c1294
Rajesh-Sharma-git <rajesh.sharma.personal@gmail.com>
2024-06-10 21:07:01 +0530

    ExploratoryAnalysis
    ExploratoryAnalysis

---
+++ Code/ExploratoryDataAnalysis/EDA.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import scipy.stats as stats
+import requests
+from io import StringIO
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Load the data
+file_path = 'D:\Rajesh\Rajesh\Personal\AISanDiego\ProjectDocuments\DiabetesHealthIndicators\GitHubClone\Diabetes-Health-Indicator\data\diabetes_012_health_indicators_BRFSS2015.csv'
+print("file_path = ", file_path)
